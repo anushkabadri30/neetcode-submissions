118ba160b332fed95a43a356ccffd982fae00f92
Anushka Badri <100365912+anushkabadri30@users.noreply.github.com>
2026-08-19 09:06:48 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+      if (s1.length()>s2.length()){
+        return false;
+
+      } 
+      int s1Count[]=new int[26];
+      int s2Count[]=new int[26];
+
+      for(char c:s1.toCharArray()){
+        s1Count[c-'a']++;
+
+      } 
+      int left=0;
+      for(int right=0;right<s2.length();right++){
+        s2Count[s2.charAt(right)-'a']++;
+
+      if(right-left+1>s1.length()){
+        s2Count[s2.charAt(left)-'a']--;
+        left++;
+      }
+      if(right-left+1==s1.length()){
+        if(matches(s1Count,s2Count)){
+            return true;
+        }
+      }
+    }
+    return false;
+}
+      private boolean matches(int[] s1Count,int[] s2Count){
+        for(int i=0;i<26;i++){
+            if(s1Count[i]!=s2Count[i]){
+                return false;
+            }
+
+        }
+        return true;
+
+      }
+}
+
